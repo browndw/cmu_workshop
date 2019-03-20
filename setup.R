ff43caa2d04be0a37a1e6eb4d14245338131529e
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-03-20 11:30:35 -0400

    setup code for non-Binder users

---
+++ setup.R
@@ -0,0 +1,8 @@
+# Run this code to install packages that we'll be using during the workshops.
+#
+# You ONLY need to do this if you are runniing R and Rstudio on your own laptop.
+# If you are using the Binder web interface, these have already been installed
+# for you
+
+install.packages(c("tidyverse", "quanteda", "ggrepel"))
+devtools::install_github("rlib/rematch2")
